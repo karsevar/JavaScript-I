24b59f97e9454d8374e5bb2de8e8ec47d94c64c4
masonkarsevar <masonkarsevar@gmail.com>
2019-06-10 12:36:21 -0700

    MVP: arrays.js and objects.js finished. WIP: function-conversion.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -46,7 +46,7 @@ const gannie = {
 	'gender': 'M'
 }
 
-const Antonietta = {
+const antonietta = {
 	'id': 5,
 	'name': 'Antonietta',
 	'email': 'adaine5@samsung.com',
@@ -58,21 +58,38 @@ const Antonietta = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(mitzi.name);
 
 // Kennan's ID
+console.log(kennan.id);
 
 // Keven's email
+console.log(keven.email);
 
 // Gannie's name
+console.log(gannie.name);
 
 // Antonietta's Gender
+console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+kennan.speak = function() {
+	console.log(`Hello, my name is ${this.name}`)
+}
+
+kennan.speak();
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+antonietta.multiplyNums = function(num1, num2) {
+	return num1 * num2;
+}
+
+console.log(antonietta.multiplyNums(3, 4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
